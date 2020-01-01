cdb8580f4eb853d9f27e978e0744ad0ab2b08124
ocnkr <onurcankur96@gmail.com>
2020-01-01 17:43:40 +0300

    Approve and allowance functions are added with the allowance mapping.

--- erc-1155/contracts/ERC1155.sol
+++ erc-1155/contracts/ERC1155.sol
@@ -17,6 +17,43 @@ contract ERC1155 is IERC1155, ERC165, CommonConstants
 
     // owner => (operator => approved)
     mapping (address => mapping(address => bool)) internal operatorApproval;
+    
+    // from => operator => id => allowance
+    mapping(address => mapping(address => mapping(uint256 => uint256))) allowances;
+/////////////////////////////////////////// APPROVE //////////////////////////////////////////////    
+    /**
+        @dev MUST emit on any successful call to approve(address _spender, uint256 _id, uint256 _currentValue, uint256 _value)
+    */
+    event Approval(address indexed _owner, address indexed _spender, uint256 indexed _id, uint256 _oldValue, uint256 _value);
+
+    /**
+        @notice Allow other accounts/contracts to spend tokens on behalf of msg.sender
+        @dev MUST emit Approval event on success.
+        To minimize the risk of the approve/transferFrom attack vector (see https://docs.google.com/document/d/1YLPtQxZu1UAvO9cZ1O2RPXBbT0mooh4DYKjA_jp-RLM/), this function will throw if the current approved allowance does not equal the expected _currentValue, unless _value is 0.
+        @param _spender      Address to approve
+        @param _id           ID of the Token
+        @param _currentValue Expected current value of approved allowance.
+        @param _value        Allowance amount
+    */
+    function approve(address _spender, uint256 _id, uint256 _currentValue, uint256 _value) external {
+
+        require(allowances[msg.sender][_spender][_id] == _currentValue);
+        allowances[msg.sender][_spender][_id] = _value;
+        
+        emit Approval(msg.sender, _spender, _id, _currentValue, _value);
+    }
+
+    /**
+        @notice Queries the spending limit approved for an account
+        @param _id       ID of the Token
+        @param _owner    The owner allowing the spending
+        @param _spender  The address allowed to spend.
+        @return          The _spender's allowed spending balance of the Token requested
+     */
+    function allowance(address _owner, address _spender, uint256 _id) external view returns (uint256){
+        return allowances[_owner][_spender][_id];
+    }
+    
 
 /////////////////////////////////////////// ERC165 //////////////////////////////////////////////
 
