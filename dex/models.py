7cf26ea8263c049a1b36659cf0f1ac72ed76cb52
Hasan Akalp <hasan.akalp@gmail.com>
2020-01-02 15:10:35 +0300

    Verbose name eklendi, Token modeline is_nf eklendi

--- dex/models.py
+++ dex/models.py
@@ -4,8 +4,8 @@ from django.urls import reverse
 
 class Game(models.Model):
     name = models.CharField(max_length=255)
-    img = models.ImageField(upload_to='game', default='game/default_game.png')
-    desc = models.TextField(null=True, blank=True)
+    img = models.ImageField(upload_to='game', default='game/default_game.png', verbose_name="Image")
+    desc = models.TextField(null=True, blank=True, verbose_name="Description")
 
     def __str__(self):
         return self.name
@@ -13,18 +13,19 @@ class Game(models.Model):
 
 class Token(models.Model):
     name = models.CharField(max_length=255)
-    img = models.ImageField(upload_to='token', default='token/default_token.jpg')
+    img = models.ImageField(upload_to='token', default='token/default_token.jpg', verbose_name="Image")
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    is_nf = models.BooleanField(default=True, verbose_name="Is Non-Fungible?")
 
     def __str__(self):
         return self.game.name + ' - ' + self.name
 
 
 class Order(models.Model):
-    usr_addr = models.CharField(max_length=42)
-    obj = models.ForeignKey(Token, on_delete=models.CASCADE)
-    value = models.CharField(max_length=255)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    usr_addr = models.CharField(max_length=42, verbose_name="User Address")
+    obj = models.ForeignKey(Token, on_delete=models.CASCADE, verbose_name="Token")
+    value = models.CharField(max_length=255, verbose_name="ETH Value")
+    timestamp = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
 
 
 class SellOrder(Order):
